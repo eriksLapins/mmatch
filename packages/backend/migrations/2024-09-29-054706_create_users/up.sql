5e23a31806ac9a3a769f36d5e648e50812ad2ea6
Ēriks Lapiņš <lapinseriks98@gmail.com>
2024-09-29 09:25:15 +0300

    added initial user table sql mgiration

---
+++ packages/backend/migrations/2024-09-29-054706_create_users/up.sql
@@ -0,0 +1,20 @@
+-- Your SQL goes here
+
+CREATE TYPE user_types AS ENUM ('Musician', 'Manager', 'Explorer');
+
+CREATE TABLE users (
+    id TEXT PRIMARY KEY,
+    name VARCHAR(125) NOT NULL,
+    lastname VARCHAR(125) NOT NULL,
+    description TEXT NOT NULL,
+    email TEXT NOT NULL,
+    password TEXT NOT NULL,
+    phone VARCHAR(125) NOT NULL,
+    phone_prefix VARCHAR(125) NOT NULL,
+    country VARCHAR(125) NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    street VARCHAR(255) NOT NULL,
+    house_number VARCHAR(16) NOT NULL,
+    apartment VARCHAR(16),
+    types user_types ARRAY NOT NULL
+)
